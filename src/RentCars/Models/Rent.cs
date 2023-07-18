9ef6817d6ce8b017a905cd84cb9ca57881473d13
Priscila <priscilasartori2@gmail.com>
2023-07-18 18:08:43 -0300

    feat: Crie o construtor de Rent seguindo as regras de negócio

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -4,27 +4,42 @@ namespace RentCars.Models;
 
 public class Rent
 {
-    public Vehicle Vehicle { get; set; }
-    public Person Person { get; set; }
-    public int DaysRented { get; set; }
-    public double Price { get; set; }
-    public RentStatus Status { get; set; }
+  public Vehicle Vehicle { get; set; }
+  public Person Person { get; set; }
+  public int DaysRented { get; set; }
+  public double Price { get; set; }
+  public RentStatus Status { get; set; }
 
-    //10 - Crie o construtor de `Rent` seguindo as regras de negócio
-    public Rent(Vehicle vehicle, Person person, int daysRented)
+  //10 - Crie o construtor de `Rent` seguindo as regras de negócio
+  public Rent(Vehicle vehicle, Person person, int daysRented)
+  {
+    Vehicle = vehicle;
+    Person = person;
+    DaysRented = daysRented;
+
+    if (person is PhysicalPerson)
     {
-        throw new NotImplementedException();
+      Price = vehicle.PricePerDay * daysRented;
+    }
+    else
+    {
+      Price = (Price * daysRented) - (Price * daysRented * 0.10);
     }
 
-    //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
-    public void Cancel()
-    {
-        throw new NotImplementedException();
-    }
+    Status = RentStatus.Confirmed;
+    Vehicle.IsRented = true;
+    Person.Debit = Price;
+  }
 
-    //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
-    public void Finish()
-    {
-        throw new NotImplementedException();
-    }
+  //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
+  public void Cancel()
+  {
+    throw new NotImplementedException();
+  }
+
+  //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
+  public void Finish()
+  {
+    throw new NotImplementedException();
+  }
 }
